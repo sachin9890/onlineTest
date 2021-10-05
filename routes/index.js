535a9659ef154511e859a5a22e4be2c6810fd5d7
Boss <boss@Machitosh.local>
2021-10-05 15:47:41 +0530

    Fixed cors issue

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ var router = express.Router();
 
 const {MongoClient} = require('mongodb');
 const uri = "mongodb+srv://sachin:sachin721@cluster0.qcgao.mongodb.net";
-
-
-  const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
   try {
       // Connect to the MongoDB cluster
-      var con = client.connect();
+      client.connect();
       
       // Make the appropriate DB calls
       // await  listDatabases(client);
@@ -23,6 +21,32 @@ const uri = "mongodb+srv://sachin:sachin721@cluster0.qcgao.mongodb.net";
         })
         
       });
+
+      router.post('/submit', function(req, res, next) {
+        var result = client.db('onlineTest').collection('result').insertOne({correctAns: 22, inCorrectAns: 21, finalResult: 'pass'}).finally(function(){
+          res.header('Cache-Control', 'no-cache, no-store, must-revalidate')
+          res.json({correctAns: 22, inCorrectAns: 21, finalResult: 'pass'})
+        })
+        // .toArray(function(err, result){
+        //   var answers = result;
+        //   var correctAns = 0;
+        //   var inCorrect = 0;
+
+          // req.body.map(function(ele, index){
+          //     var checkAns = answers.find(function(item, index){
+          //       return item.id === ele.id && 
+          //     })
+          // })
+          //return result
+          
+          
+          //res.json(req.body)
+
+        //})
+        
+      });
+
+
 
   } catch (e) {
       console.error(e);
